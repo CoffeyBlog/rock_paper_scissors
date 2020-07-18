d60159db5c21bde655f68a6525166873e814648b
coffey <lucascoffey@gmail.com>
2020-07-18 12:21:07 -0400

    refactored - to reduce code but has a bug

--- rpsgame.py
+++ rpsgame.py
@@ -17,10 +17,15 @@ def show_header():
 
 
 def play_game(player_one, player_two):
+    rounds = 3
+    wins_p1 = 0
+    wins_p2 = 0
+
     rolls = ['rock', 'paper', 'scissors']
 
-    roll1 = get_roll(player_one, rolls)
-    roll2 = random.choice(rolls)
+    while wins_p1 < rounds and wins_p2 < rounds:
+        roll1 = get_roll(player_one, rolls)
+        roll2 = random.choice(rolls)
 
     if not roll1:
         print("Can't play that - exiting")
@@ -29,7 +34,24 @@ def play_game(player_one, player_two):
     print(f"{player_one} rolls {roll1}")
     print(f"{player_two} rolls {roll2}")
 
-    check_for_winning_throw(player_one, player_two, roll1, roll2)
+    winner = check_for_winning_throw(player_one, player_two, roll1, roll2)
+
+    if winner is None:
+        print("This round was a tie")
+    else:
+        print(f'{winner} takes this round')
+        if winner == player_one:
+            wins_p1 += 1
+        elif winner == player_two:
+            wins_p2 += 1
+
+
+    if wins_p1 >= rounds:
+        overall_winner = player_one
+    else:
+        overall_winner = player_one
+
+    print(f"{overall_winner} wins")
 
 
 def check_for_winning_throw(player_one, player_two, roll1, roll2):
